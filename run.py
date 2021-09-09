1d112649d7998312946b90ad001ebb3145a0d29a
Joh201 <johngt632@gmail.com>
2021-09-09 09:05:33 +0000

    Add the play game function

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+from random import randint
 
 def country_file(file):
     '''
@@ -39,3 +40,24 @@ def capital_file(file):
 print(capital_file('capitals.txt'))
 
 
+def play_game():
+    '''
+    The function runs the game and asks the player to
+    input their response. 
+
+    '''
+    player_name = input('Pleae enter your name.\n')
+    print(f'Welcome to the game {player_name}!')
+    country_list = country_file('countries.txt')
+    capital_list = capital_file('capitals.txt')
+    random_index = randint(0, 4)
+    
+    print(f'What is the capital city of {country_list[random_index]}?')
+    response = input('Please enter city name. For example Asmara, Stockholm.\n')
+    print(response)
+    
+
+
+    
+    
+play_game()
